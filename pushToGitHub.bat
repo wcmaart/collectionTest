dbc0398dafe2f68d086e82eb1f511bc1851804e8
James Allison <jallison@ad.williams.edu>
2024-01-26 11:09:41 -0500

    Automated commit

---
+++ pushToGitHub.bat
@@ -0,0 +1,22 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+REM Setting up GitHub credentials
+SET GITHUB_TOKEN=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
+SET GITHUB_USER=linguaphile
+SET REPO_NAME=wcmaart/collectionTest
+
+REM Navigating to your local repository folder
+CD C:\Users\Public\feeds2\collection
+
+REM Configuring Git for the push
+git config --local credential.helper store
+
+REM Pushing to GitHub
+echo !GITHUB_USER!:!GITHUB_TOKEN! | git credential-manager store
+git add .
+git commit -m "Automated commit"
+git push https://%GITHUB_USER%:%GITHUB_TOKEN%@github.com/%GITHUB_USER%/%REPO_NAME%.git
+
+echo Push completed
+pause
